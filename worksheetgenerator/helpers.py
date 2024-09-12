954a7d681deea61863c895a79af790d01dbfd841
kitsonswann <kptswann@gmail.com>
2024-09-11 21:12:03 -0700

    Changing to 10'

--- worksheetgenerator/helpers.py
+++ worksheetgenerator/helpers.py
@@ -74,7 +74,7 @@ def generate_table_rows(images):
 
     table_rows = []
     for row in range(N_QUESTIONS_PER_PAGE):
-        left, right, answer = generate_addition_problem(max_answer=20, max_digit=19)
+        left, right, answer = generate_addition_problem(max_answer=10, max_digit=9)
         row = dbc.Row(
             [
                 dbc.Col(generate_image_arrangement(left, images=images)), 
